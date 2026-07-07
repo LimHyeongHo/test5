695c574c88538375a8081bb547b05ee708748925
형호 <super8114@naver.com>
2026-07-07 15:40:19 +0900

    feat: localhost port 번호 매칭

--- backend/src/main/java/com/Nbbang/backend/domain/product/controller/ProductController.java
+++ backend/src/main/java/com/Nbbang/backend/domain/product/controller/ProductController.java
@@ -13,7 +13,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/products")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "http://localhost:5173") 
+@CrossOrigin(origins = "http://localhost:3000") 
 public class ProductController {
 
     private final ProductService productService; // 💡 레포지토리 대신 서비스로 변경!
